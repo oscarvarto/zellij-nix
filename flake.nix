a900ff0cbae88c9e9a09742ab01bf909164cc4fd
Oscar Vargas Torres <contact@oscarvarto.mx>
2026-01-07 06:41:59 -0600

    Pointing to zellij commit 8aba80ebcc8cc22c455ebcf377ef2793c459a4e3

--- flake.nix
+++ flake.nix
@@ -13,117 +13,112 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    zellij.url = "github:zellij-org/zellij/3fe48a972c55537502128779116d38d8f8aedb7e";
+    zellij.url = "github:zellij-org/zellij/8aba80ebcc8cc22c455ebcf377ef2793c459a4e3";
     zellij.flake = false;
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      zellij,
-      multitask,
-      rust-overlay,
-      flake-utils,
-      ...
-    }:
-    let
-      src = zellij;
-      cargoTOML = builtins.fromTOML (builtins.readFile (src + "/Cargo.toml"));
-      name = cargoTOML.package.name;
-      version =
-        if builtins.isAttrs cargoTOML.package.version
+  outputs = {
+    self,
+    nixpkgs,
+    zellij,
+    multitask,
+    rust-overlay,
+    flake-utils,
+    ...
+  }: let
+    src = zellij;
+    cargoTOML = builtins.fromTOML (builtins.readFile (src + "/Cargo.toml"));
+    name = cargoTOML.package.name;
+    version =
+      if
+        builtins.isAttrs cargoTOML.package.version
         && (cargoTOML.package.version.workspace or false)
-        then cargoTOML.workspace.package.version
-        else cargoTOML.package.version;
-      cargoLock = {
-        lockFile = builtins.path {
-          path = src + "/Cargo.lock";
-          name = "Cargo.lock";
-        };
-        allowBuiltinFetchGit = true;
+      then cargoTOML.workspace.package.version
+      else cargoTOML.package.version;
+    cargoLock = {
+      lockFile = builtins.path {
+        path = src + "/Cargo.lock";
+        name = "Cargo.lock";
       };
-      make-zellij =
-        {
-          makeRustPlatform,
-          lib,
-          stdenv,
-          pkg-config,
-          protobuf,
-          openssl,
-          perl,
-          rust-bin,
-          darwin,
-          patchPlugins ? true,
-        }:
-        let
-          rustToolchainTOML = rust-bin.fromRustupToolchainFile (src + /rust-toolchain.toml);
-          rustc = rustToolchainTOML;
-          cargo = rustToolchainTOML;
-        in
-        (makeRustPlatform { inherit cargo rustc; }).buildRustPackage {
-          inherit
-            cargoLock
-            name
-            src
-            stdenv
-            version
-            ;
-          nativeBuildInputs = [
-            pkg-config
-            protobuf
-            perl
-          ];
-
-          buildInputs = [
-            openssl
-            protobuf
-          ];
-
-          patchPhase =
-            if patchPlugins then
-              ''
-                cp ${
-                  self.outputs.plugins.${stdenv.system}.configuration
-                }/bin/configuration.wasm zellij-utils/assets/plugins/configuration.wasm
-                cp ${
-                  self.outputs.plugins.${stdenv.system}.tab-bar
-                }/bin/tab-bar.wasm zellij-utils/assets/plugins/tab-bar.wasm
-                cp ${
-                  self.outputs.plugins.${stdenv.system}.status-bar
-                }/bin/status-bar.wasm zellij-utils/assets/plugins/status-bar.wasm
-                cp ${
-                  self.outputs.plugins.${stdenv.system}.strider
-                }/bin/strider.wasm zellij-utils/assets/plugins/strider.wasm
-                cp ${
-                  self.outputs.plugins.${stdenv.system}.compact-bar
-                }/bin/compact-bar.wasm zellij-utils/assets/plugins/compact-bar.wasm
-                cp ${
-                  self.outputs.plugins.${stdenv.system}.session-manager
-                }/bin/session-manager.wasm zellij-utils/assets/plugins/session-manager.wasm
-              ''
-            else
-              ":";
-          meta = {
-            description = "A terminal workspace with batteries included";
-            homepage = "https://zellij.dev/";
-            license = [ lib.licenses.mit ];
-            mainProgram = "zellij";
-          };
-        };
+      allowBuiltinFetchGit = true;
+    };
+    make-zellij = {
+      makeRustPlatform,
+      lib,
+      stdenv,
+      pkg-config,
+      protobuf,
+      openssl,
+      perl,
+      rust-bin,
+      darwin,
+      patchPlugins ? true,
+    }: let
+      rustToolchainTOML = rust-bin.fromRustupToolchainFile (src + /rust-toolchain.toml);
+      rustc = rustToolchainTOML;
+      cargo = rustToolchainTOML;
     in
+      (makeRustPlatform {inherit cargo rustc;}).buildRustPackage {
+        inherit
+          cargoLock
+          name
+          src
+          stdenv
+          version
+          ;
+        nativeBuildInputs = [
+          pkg-config
+          protobuf
+          perl
+        ];
+
+        buildInputs = [
+          openssl
+          protobuf
+        ];
+
+        patchPhase =
+          if patchPlugins
+          then ''
+            cp ${
+              self.outputs.plugins.${stdenv.system}.configuration
+            }/bin/configuration.wasm zellij-utils/assets/plugins/configuration.wasm
+            cp ${
+              self.outputs.plugins.${stdenv.system}.tab-bar
+            }/bin/tab-bar.wasm zellij-utils/assets/plugins/tab-bar.wasm
+            cp ${
+              self.outputs.plugins.${stdenv.system}.status-bar
+            }/bin/status-bar.wasm zellij-utils/assets/plugins/status-bar.wasm
+            cp ${
+              self.outputs.plugins.${stdenv.system}.strider
+            }/bin/strider.wasm zellij-utils/assets/plugins/strider.wasm
+            cp ${
+              self.outputs.plugins.${stdenv.system}.compact-bar
+            }/bin/compact-bar.wasm zellij-utils/assets/plugins/compact-bar.wasm
+            cp ${
+              self.outputs.plugins.${stdenv.system}.session-manager
+            }/bin/session-manager.wasm zellij-utils/assets/plugins/session-manager.wasm
+          ''
+          else ":";
+        meta = {
+          description = "A terminal workspace with batteries included";
+          homepage = "https://zellij.dev/";
+          license = [lib.licenses.mit];
+          mainProgram = "zellij";
+        };
+      };
+  in
     # flake outputs
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
+      system: let
+        overlays = [(import rust-overlay)];
 
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs {inherit system overlays;};
 
         rustToolchainTOML = pkgs.rust-bin.fromRustupToolchainFile (src + /rust-toolchain.toml);
         rustWasmToolchainTOML = rustToolchainTOML.override {
-          extensions = [ ];
-          targets = [ "wasm32-wasip1" ];
+          extensions = [];
+          targets = ["wasm32-wasip1"];
         };
 
         devInputs = [
@@ -157,17 +152,17 @@
           rustc = rustWasmToolchainTOML;
           cargo = rustWasmToolchainTOML;
         };
-      in
-      rec {
+      in rec {
         packages = rec {
           # The default build compiles the plugins from src
           default = zellij;
-          zellij = pkgs.callPackage make-zellij { };
+          zellij = pkgs.callPackage make-zellij {};
           # The upstream build relies on precompiled binary plugins that are included in the upstream src
-          zellij-upstream = pkgs.callPackage make-zellij { patchPlugins = false; };
+          zellij-upstream = pkgs.callPackage make-zellij {patchPlugins = false;};
         };
         plugins = {
-          inherit (defaultPlugins)
+          inherit
+            (defaultPlugins)
             compact-bar
             configuration
             session-manager
@@ -179,8 +174,8 @@
         };
 
         apps = {
-          default = flake-utils.lib.mkApp { drv = packages.default; };
-          zellij-upstream = flake-utils.lib.mkApp { drv = packages.zellij-upstream; };
+          default = flake-utils.lib.mkApp {drv = packages.default;};
+          zellij-upstream = flake-utils.lib.mkApp {drv = packages.zellij-upstream;};
         };
 
         devShells = {
@@ -189,13 +184,14 @@
             nativeBuildInputs = devInputs;
             RUST_BACKTRACE = 1;
           };
-          fmtShell = pkgs.mkShell { buildInputs = fmtInputs; };
-          actionlintShell = pkgs.mkShell { buildInputs = [ pkgs.actionlint ]; };
+          fmtShell = pkgs.mkShell {buildInputs = fmtInputs;};
+          actionlintShell = pkgs.mkShell {buildInputs = [pkgs.actionlint];};
         };
 
         checks = {
           inherit (self.outputs.packages.${system}) default zellij-upstream;
-          inherit (self.outputs.plugins.${system})
+          inherit
+            (self.outputs.plugins.${system})
             compact-bar
             configuration
             session-manager
@@ -211,12 +207,12 @@
     // {
       overlays = {
         default = final: _: {
-          zellij = final.callPackage make-zellij { };
-          zellij-upstream = final.callPackage make-zellij { patchPlugins = false; };
+          zellij = final.callPackage make-zellij {};
+          zellij-upstream = final.callPackage make-zellij {patchPlugins = false;};
         };
         nightly = final: _: {
-          zellij-nightly = final.callPackage make-zellij { };
-          zellij-upstream-nightly = final.callPackage make-zellij { patchPlugins = false; };
+          zellij-nightly = final.callPackage make-zellij {};
+          zellij-upstream-nightly = final.callPackage make-zellij {patchPlugins = false;};
         };
       };
     };
